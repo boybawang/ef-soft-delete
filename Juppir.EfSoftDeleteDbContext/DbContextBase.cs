49236b8a9b91771abad92b6c10ea4ebc09a36364
boybawang <edison.capiz@gmail.com>
2014-02-28 11:40:52 +0800

    Initial Commit

---
+++ Juppir.EfSoftDeleteDbContext/DbContextBase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juppir.EfSoftDeleteDbContext
+{
+    public class DbContextBase : DbContext
+    {
+        static DbContextBase()
+        {
+            Database.SetInitializer<DbContextBase>(null);
+        }
+        public DbContextBase(string connectionStringName)
+            : base(connectionStringName)
+        {
+
+        }
+
+        public override int SaveChanges()
+        {
+            var entries = ChangeTracker.Entries().Where(p => p.State == EntityState.Deleted);
+            foreach (var entry in entries)
+            {
+                SoftDelete(entry);
+            }
+
+            return base.SaveChanges();
+        }
+
+        private void SoftDelete(DbEntityEntry entry)
+        {
+            var deletedProperty = entry.Property("Deleted");
+            if (deletedProperty == null)
+                deletedProperty = entry.Property("IsDeleted");
+            if (deletedProperty != null)
+            {
+                deletedProperty.CurrentValue = true;
+                entry.State = EntityState.Modified;
+            }
+        }
+
+    }
+}
